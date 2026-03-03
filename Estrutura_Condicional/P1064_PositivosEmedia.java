b26a99893b3729f99a68a4d6cb6e92832b9e6450
Gabriel Galvão <gabrielgalvao95@gmail.com>
2026-03-03 12:49:39 -0300

    Aciona Exercício 1064 - Positivos em Media

---
+++ Estrutura_Condicional/P1064_PositivosEmedia.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class P1064_PositivosEmedia {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int positivos = 0;
+		double soma = 0;
+
+		for (int i = 0; i < 6; i++) {
+			double valor = sc.nextDouble();
+
+			if (valor > 0) {
+				positivos++;
+				soma = soma + valor;
+			}
+
+		}
+
+		double media = soma / positivos;
+
+		System.out.println(positivos + " valores positivos");
+		System.out.printf("%.1f%n", media);
+
+		sc.close();
+
+	}
+
+}
