fe40db121ddb704bb33a57b4a7ce71f40ffd72e5
Gabriel Galvão <gabrielgalvao95@gmail.com>
2026-03-03 12:18:01 -0300

    Aciona Exercício 1060 - Numeros Positivos

---
+++ Estrutura_Condicional/P1060_NumerosPositivos.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class P1060_NumerosPositivos {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int positivos = 0;
+		
+		for (int i = 0; i < 6; i++) {
+			double valor = sc.nextDouble();
+			
+			if (valor > 0) {
+				positivos++;
+			}
+		}
+		System.out.println(positivos + " valores positivos");
+		
+		sc.close();
+	}
+
+}
